a2777ba1e83fd53992957541752f54f8a19f10b8
Álvaro Palma <palma.alvaro@outlook.com>
2022-08-03 19:24:05 -0300

    chore: initial config and dependencies install

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next/types'
+
+const Home: NextPage = () => {
+  return <h1>Hello World</h1>
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/features/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(var(--primary) / 1 )',
+        secondary: 'rgb(var(--secondary) / 1 )',
+        dark: 'rgb(var(--dark) / 1 )',
+        light: 'rgb(var(--light) / 1 )',
+        success: 'rgb(var(--success) / 1 )',
+        info: 'rgb(var(--info) / 1 )',
+        warning: 'rgb(var(--warning) / 1 )',
+        danger: 'rgb(var(--danger) / 1 )',
+      },
+      fontFamily: {
+        custom: ['ubuntu-mono', 'Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
