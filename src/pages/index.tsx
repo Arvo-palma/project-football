e164bf8995144feba5e8d05def50f1a74be92f82
Álvaro Palma <palma.alvaro@outlook.com>
2022-08-03 23:11:20 -0300

    feat: header layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import HomeScreen from 'components/screens/home/home-screen'
 import { NextPage } from 'next/types'
 
 const HomeRoute: NextPage = () => {
-  return <HomeScreen />
+  return <HomeScreen className="dark" />
 }
 
 export default HomeRoute
