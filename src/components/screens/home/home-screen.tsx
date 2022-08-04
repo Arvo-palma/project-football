68f46af51d237c73a86ef8baa1ab657a1dea2185
Álvaro Palma <palma.alvaro@outlook.com>
2022-08-04 00:26:12 -0300

    refactor: classNames reponsability of home screen and header

--- src/components/screens/home/home-screen.tsx
+++ src/components/screens/home/home-screen.tsx
@@ -1,13 +1,20 @@
-import DefaultHeader from 'components/templates/default-header/default-header'
+import CentralContainer from 'components/templates/central-container/central-container'
+import Header from 'components/templates/header/header'
+import { classNames } from 'core/helpers/class-names'
 import React from 'react'
 
 type HomeScreenProps = {
   className?: string
 }
-const HomeScreen = ({ className }: HomeScreenProps) => {
+const HomeScreen = ({}: HomeScreenProps) => {
   return (
-    <div className="w-full h-full bg-black bg-cover bg-opacity-60">
-      <DefaultHeader className={className} />
+    <div className="w-full bg-black bg-opacity-60 flex flex-col min-h-screen min-w-full">
+      <Header className={classNames('border-b-2 border-black')} />
+      <CentralContainer className="w-3/4 bg-black bg-opacity-70 mx-auto min-h-screen items-center">
+        <div className="flex-auto">
+          <span className="text-white">Inserir Conteúdo</span>
+        </div>
+      </CentralContainer>
     </div>
   )
 }
