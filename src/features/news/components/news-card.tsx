26ee20dd09c863c78a4d0e588ad8d60699bde71d
Álvaro Palma <palma.alvaro@outlook.com>
2022-08-05 10:11:20 -0300

    feat: news links integrated

--- src/features/news/components/news-card.tsx
+++ src/features/news/components/news-card.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { Column, Row } from 'components/toolkit'
+import Link from 'components/toolkit/link'
 import { classNames, defaultImage } from 'core/helpers'
 import { News } from 'core/types/news'
 import React from 'react'
@@ -10,26 +11,28 @@ type NewsCardProps = {
 }
 const NewsCard = ({ className, news }: NewsCardProps) => {
   return (
-    <Row
-      className={classNames(
-        'border-white border p-5 rounded-xl border-opacity-10 justify-between',
-        className
-      )}
-    >
-      <Column>
-        <Row className="text-blue-300 pb-1">{news.title}</Row>
-        <Row className="text-sm text-opacity-70 text-gray-300">
-          {news.broadcaster} - {news.updateTime}
-        </Row>
-      </Column>
-      <Column>
-        <img
-          src={news.image ? news.image : defaultImage}
-          alt={`${news.title} image`}
-          className="rounded-xl w-40 max-w-md"
-        />
-      </Column>
-    </Row>
+    <Link href={`https://www.google.com/search?cs=1&q=${news.title}`}>
+      <Row
+        className={classNames(
+          'border-white border p-5 rounded-xl border-opacity-10 justify-between',
+          className
+        )}
+      >
+        <Column>
+          <Row className="text-blue-300 pb-1 hover:underline">{news.title}</Row>
+          <Row className="text-sm text-opacity-70 text-gray-300">
+            {news.broadcaster} - {news.updateTime}
+          </Row>
+        </Column>
+        <Column>
+          <img
+            src={news.image ? news.image : defaultImage}
+            alt={`${news.title} image`}
+            className="rounded-xl w-40 max-w-md"
+          />
+        </Column>
+      </Row>
+    </Link>
   )
 }
 
