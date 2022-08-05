aff5d31d825630a53937075727283cd89c6657c7
Álvaro Palma <palma.alvaro@outlook.com>
2022-08-04 22:18:23 -0300

    feat: Match card list component

--- src/features/matches/components/matches-list.tsx
+++ src/features/matches/components/matches-list.tsx
@@ -1,10 +1,19 @@
+import { Column } from 'components/toolkit'
+import { mockedMatchesInfo } from 'core/helpers'
 import React from 'react'
+import MatchCard from './match-card'
 
 type MatchesListProps = {
   className?: string
 }
 const MatchesList = ({}: MatchesListProps) => {
-  return <></>
+  return (
+    <Column>
+      {mockedMatchesInfo.map((match, index) => (
+        <MatchCard key={index} match={match} />
+      ))}
+    </Column>
+  )
 }
 
 export default MatchesList
