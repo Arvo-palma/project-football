650e3bcd7fa6b6fe7482b85b02e6e657da7d9030
Álvaro Palma <palma.alvaro@outlook.com>
2022-08-08 16:08:30 -0300

    feat: change players request to use react query

--- src/features/players/components/players-list.tsx
+++ src/features/players/components/players-list.tsx
@@ -1,32 +1,24 @@
-import axios from 'axios'
 import PlayerCard from 'features/players/components/player-card'
 import Row from 'components/toolkit/row'
-import { classNames, handleError } from 'core/helpers'
+import { classNames } from 'core/helpers'
 import { PlayerInfo } from 'core/types/player-info'
 import React from 'react'
 import Link from 'components/toolkit/link'
+import { useGetAllPlayers } from '../hooks/use-get-all-players'
+import { Column, Spinner } from 'components/toolkit'
 
 type PlayersListProps = {
   className?: string
 }
 const PlayersList = ({ className }: PlayersListProps) => {
-  const [data, setData] = React.useState([])
-  const playersURL = 'https://api.cartola.globo.com/mercado/destaques'
+  const { data, isLoading } = useGetAllPlayers()
 
-  const getPlayers = async () => {
-    try {
-      const response = await axios.get(playersURL)
-      setData(response.data)
-    } catch (error) {
-      throw handleError(error)
-    }
-  }
-
-  React.useEffect(() => {
-    getPlayers()
-  }, [])
-
-  return (
+  return isLoading ? (
+    <Column className={classNames('items-center')}>
+      <Spinner color="black" size={100} />
+      Carregando...
+    </Column>
+  ) : (
     <Row className={classNames('flex-wrap py-1 pl-1', className)}>
       {data.map((player: PlayerInfo) => (
         <Link
